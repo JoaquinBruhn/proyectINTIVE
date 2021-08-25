fce539ce0fe9e86fbefc39fd6ebbfa4cfb8f8fa0
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-08-25 18:07:43 -0300

    creating the framework for the fetch function

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -1,11 +1,38 @@
 //creating the function for the first 50 users
-function first50(){
-    fetch("https://randomuser.me/api/?results=50")
+function first52(){
+    fetch("https://randomuser.me/api/?results=52")
         .then(res =>res.json())
         .then(data=>{
-            //testing the fetch
+            //testing the fetch and seting it up to show the array with it´s users
+            // let users = data.results;
+            // console.log(users);
+
             let users = data.results;
-            console.log(users);
+
+            let searchedResult = "<h1>Users in the platform</h1>" 
+
+            let i =0
+            //framework for the "forEach loop"
+            users.forEach(function (lists) {
+                if (i % 4 == 0){
+                    searchedResult += `
+                    <div class="container-fluid">
+                        <div class="row">
+                    `
+                };
+                searchedResult += `
+
+                `
+                i+1;
+                if (i % 4 == 0){
+                    searchedResult += `
+                    <div class="container-fluid">
+                        <div class="row">
+                    `
+                };
+            });
+
+            document.getElementById('searchedResult').innerHTML = searchedResult;
         })
 }
-first50()
+first52()
