3c591a9c1b702698ff9e8cf3e24d83b66e2a6f4b
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-08-29 13:15:09 -0300

    modified the main JS to have it´s functios modulated need to add and update comments

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -1,12 +1,11 @@
+// import {createCard} from "./utils/cards";
 //array with all the loaded users displayed
 let usersDatabase = [];
-//variable that´s goint to turn into the ID of the user that was clicked and used as a reference in the index of usersDatabase[]
-let userID
-//count the amount of times the function users52 has been used and allows it to be reused multiple times so that the list of users is practicaly infinite
-let t = 0
-//used to asign an ID to the loaded users
-let i = 0
 
+
+users52()
+
+//button that requests 52 users
 let mainButton = document.getElementById("findUsers")
     mainButton.addEventListener("click", users52)
 
@@ -16,71 +15,17 @@ function users52(){
     fetch("https://randomuser.me/api/?results=52")
         .then(res =>res.json())
         .then(data=>{
-
+        
+            console.log("hola")
             //testing the fetch and setting it up to show the array with it´s users
             let users = data.results;
             console.log(users);
             
-            
-
-            //forEach loop that creates the HTML list of users along with filling up the array usersDatabase []
-            users.forEach(function (lists) {
-
-                //tags that contain the User Data
-                let mainSlot = document.createElement('div');
-                mainSlot.className = 'col-md-3';
-
-                let user = document.createElement('div');
-                user.className = 'card mt-4 bg-light';
-                user.idName = `${i}`;
-
-
-                //user individual data that´s going to be grouped up 
-                let userName = document.createElement(`li`);
-                userName.className = `list-group-item userName`;
-                userName.innerHTML = `<h4>Name: ${lists.name.first} ${lists.name.last}</h4>`;
-
-                let userImgSlot = document.createElement(`li`);
-                userImgSlot.className = `list-group-item thumbnailImg`;
-                    
-                    //setting the thmbnail IMG as a button and giving each button an event listener with a specific ID
-                    let userImgBtn = document.createElement(`button`);
-                    userImgBtn.className = `btn moreInfoBtn`;
-                    userImgBtn.innerHTML = `<img src="${lists.picture.medium}">`;
-                    userImgBtn.idName = `${i}`;
-                    userImgBtn.addEventListener(`click`,() => {
-                        userID = parseInt(userImgBtn.idName)
-                        moreInfo()
-                    });
-
-                    userImgSlot.appendChild(userImgBtn)
-
-                let userCity = document.createElement(`li`);
-                userCity.className = `list-group-item userCity`;
-                userCity.innerHTML = `<h5>City: ${lists.location.city}</h5>`;
-
-                let userCountry = document.createElement(`li`);
-                userCountry.className = `list-group-item userCountry`;
-                userCountry.innerHTML = `<h5>Country: ${lists.location.country}</h5>`;
-
-                //pushes each pulled user from the API into an array with the position on the index matching the button ID
-                usersDatabase.push(users[i - 52*t])
-                //Adds 1 to ibefore the next loop to give the next user a different ID the use of "t" represents the amount of times the function users52 has been used
-                i=i+1
-
-                //groups ul individual data inside a div with a card class
-                user.appendChild(userName)
-                user.appendChild(userImgSlot)
-                user.appendChild(userCity)
-                user.appendChild(userCountry)
-                
-                //gets the user into a Bootstrap column for easier editing of style
-                mainSlot.appendChild(user)
-                //All columns of searched users get into a bootstarp ROW DIV 
-                searchedResult.appendChild(mainSlot)
-            });
-            //Count the amount of times the function users52 has been used
-            t = t + 1
+            usersDatabase = usersDatabase.concat(data.results)
+            ix=usersDatabase.length - 52
+            for (let i=ix; i<usersDatabase.length; i++){
+                createCard(usersDatabase[i],i)
+            }
     })
 }
 
@@ -91,3 +36,55 @@ function moreInfo(){
 
     window.open("pages/moreInfo.html", "_blank");
 }
+
+
+//forEach loop that creates the HTML list of users along with filling up the array usersDatabase []
+function createCard(userData,i){
+    //tags that contain the User Data
+    let mainSlot = document.createElement('div');
+    mainSlot.className = 'col-md-3';
+
+    user = document.createElement('div');
+    user.className = 'card mt-4 bg-light';
+
+
+    //user individual data that´s going to be grouped up 
+    let userName = document.createElement(`li`);
+    userName.className = `list-group-item userName`;
+    userName.innerHTML = `<h4>Name: ${userData.name.first} ${userData.name.last}</h4>`;
+
+    let userImgSlot = document.createElement(`li`);
+    userImgSlot.className = `list-group-item thumbnailImg`;
+        
+        //setting the thmbnail IMG as a button and giving each button an event listener with a specific ID
+        let userImgBtn = document.createElement(`button`);
+        userImgBtn.className = `btn moreInfoBtn`;
+        userImgBtn.innerHTML = `<img src="${userData.picture.medium}">`;
+        userImgBtn.idName = `${i}`;
+        userImgBtn.addEventListener(`click`,() => {
+            userID = parseInt(userImgBtn.idName)
+            moreInfo()
+        });
+
+        userImgSlot.appendChild(userImgBtn)
+
+    let userCity = document.createElement(`li`);
+    userCity.className = `list-group-item userCity`;
+    userCity.innerHTML = `<h5>City: ${userData.location.city}</h5>`;
+
+    let userCountry = document.createElement(`li`);
+    userCountry.className = `list-group-item userCountry`;
+    userCountry.innerHTML = `<h5>Country: ${userData.location.country}</h5>`;
+
+
+    //groups ul individual data inside a div with a card class
+    user.appendChild(userName)
+    user.appendChild(userImgSlot)
+    user.appendChild(userCity)
+    user.appendChild(userCountry)
+    
+    //gets the user into a Bootstrap column for easier editing of style
+    mainSlot.appendChild(user)
+    //All columns of searched users get into a bootstarp ROW DIV 
+    searchedResult.appendChild(mainSlot)
+};
