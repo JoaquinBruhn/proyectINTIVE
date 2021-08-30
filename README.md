08fbf46f7df9bc873457bf00fa237ca951f6da5b
Joaquin Bruhn <joaquin_bruhn@hotmail.com.ar>
2021-08-29 23:34:23 -0300

    all done, code clean and working, readme finished and comments fully added

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # proyectINTIVE
 
-placeholder
+The app is intuitive to run, the page loads and executes the JS file wich automaticaly brings 52 users from the API and loads them into the site. At the same time it stores each of the 52 users on an array (usersDatabase[]) , It also asigns each button(wich contains the thumbnail) and ID wich corresponds with it´s placement on usersDatabase[]´s index wich will help later when clicking on a thumbnail to get more info on a new tab.
+
+If you are wondering about why the number 52 is so that the user´s cards could be placed in columns of 4 wich I thought would look more apealing. I just needed a multiple of 4 that´s bigger than 50 wich was the minimum amount of users that you requested.
+
+When you click on any of the card´s thumbnail the main JS file runs a function that saves that user´s information on the sesion storage. I used to load the entire array with along with the ID but I changed it up to just that user´s data since it was a waste of storage capacity. As it was requested a new tab opens (moreInfo.html) that is linked to the second js file (details.js). This JS automaticaly runs a function that pulls the information back from the sesion storage, and uses it to fill in an HTML template. This card has a bigger picture and more details about that user, I used as many as I thought that would fit without affecting much how it looked.
+
+At the bottom of the site there is a button that allows you to load 52 more users, it can be reused as many times as you want making it infinite. This new users get added into usersdatabase[] and are also asigned an ID this is all done automaticaly and doesn´t break the site´s functionality in any way.
+
+More details about how it works can be seen on the comments of the JS files and a bit on the HTML files
+
+--------------------------------
+
+I apologise for ignoring the part of the challenge that requests doing it in React or Angular I tried to speed learn React this week but I couldn´t master it enough to get something presentable. At the same time I made just in case this vanilla version as a back up wich satisfies all the other conditions. 
+
+On another note the searchbar and filters ended up as placeholders. I put them there at the start when I was making the layout with on index.html as to not make it look that simple but I run out of time and cuoldn´t actualy implement them. They are left there just couse they fit the design.
--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -1,27 +1,29 @@
-// import {createCard} from "./utils/cards";
 //array with all the loaded users displayed
 let usersDatabase = [];
 
-
+//automaticaly runs the function thefirst time and get´s the first 52 users
 users52()
+//This is used to make the Zoom In effect work
+AOS.init();
 
-//button that requests 52 users
+//button that requests 52 extra users
 let mainButton = document.getElementById("findUsers")
     mainButton.addEventListener("click", users52)
 
 //creating the function for that obtains 52 users
 function users52(){
-    //fetches the data from the site, the number can be altered from the link
+    //fetches the data from the site
     fetch("https://randomuser.me/api/?results=52")
         .then(res =>res.json())
         .then(data=>{
-        
-            console.log("hola")
-            //testing the fetch and setting it up to show the array with it´s users
-            let users = data.results;
-            console.log(users);
+
+            //testing the fetch and setting it up to show the array with the users pulled from the API
+            console.log(data.results);
             
+            //adds the incoming users to the main array, it also add the new ones to the already existing ones if it´s requested again
             usersDatabase = usersDatabase.concat(data.results)
+            //"i" is going to be used to represent the index value on our usersDatabas[] marking wich user is going to be loaded on each loop. It also gives unique ID to each user´s thumbnail button.
+            //this equation allows the function to be used again while always generating a new ID and also not reload users from the first search again when loading a second one.
             ix=usersDatabase.length - 52
             for (let i=ix; i<usersDatabase.length; i++){
                 createCard(usersDatabase[i],i)
@@ -29,62 +31,64 @@ function users52(){
     })
 }
 
-function moreInfo(){
-
-    //obtaining and saving the data from the pecific selected user in the array to save storage space
-    sessionStorage.setItem("searchedUser",JSON.stringify(usersDatabase[userID]))
-
-    window.open("pages/moreInfo.html", "_blank");
-}
-
-
-//forEach loop that creates the HTML list of users along with filling up the array usersDatabase []
+//Each loop creates the HTML div of a new user on the array usersDatabase []
 function createCard(userData,i){
-    //tags that contain the User Data
+    //tags that contain the User´s Data
     let mainSlot = document.createElement('div');
     mainSlot.className = 'col-xl-3 col-lg-6';
+    mainSlot.dataset.aos = `zoom-in`
 
     user = document.createElement('div');
     user.className = 'card-body mt-4 bg-light users__cards';
 
 
-    //user individual data that´s going to be grouped up 
-    let userName = document.createElement(`li`);
-    userName.className = `list-group-item userName`;
-    userName.innerHTML = `<h4>Name: ${userData.name.first} ${userData.name.last}</h4>`;
+        //user´s individual data that´s going to be grouped up inside the Bootstrap card 
+        let userName = document.createElement(`li`);
+        userName.className = `list-group-item userName`;
+        userName.innerHTML = `<h4>Name: ${userData.name.first} ${userData.name.last}</h4>`;
 
-    let userImgSlot = document.createElement(`li`);
-    userImgSlot.className = `list-group-item thumbnailImg`;
-        
-        //setting the thmbnail IMG as a button and giving each button an event listener with a specific ID
-        let userImgBtn = document.createElement(`button`);
-        userImgBtn.className = `btn rounded-circle moreInfoBtn`;
-        userImgBtn.innerHTML = `<img class="img-fluid rounded-circle" src="${userData.picture.medium}">`;
-        userImgBtn.idName = `${i}`;
-        userImgBtn.addEventListener(`click`,() => {
-            userID = parseInt(userImgBtn.idName)
-            moreInfo()
-        });
+        let userImgSlot = document.createElement(`li`);
+        userImgSlot.className = `list-group-item thumbnailImg`;
+            
+            //setting the thmbnail IMG as a button and giving each button an event listener with a specific ID
+            let userImgBtn = document.createElement(`button`);
+            userImgBtn.className = `btn rounded-circle moreInfoBtn`;
+            userImgBtn.innerHTML = `<img class="img-fluid rounded-circle" src="${userData.picture.medium}">`;
+            userImgBtn.idName = `${i}`;
+            userImgBtn.addEventListener(`click`,() => {
+                //when the thumbnail is clicked it turns the id from a sring to a number and saves it as userID then it runs the function to open it on a new tab
+                userID = parseInt(userImgBtn.idName)
+                moreInfo()
+            });
 
-        userImgSlot.appendChild(userImgBtn)
+            userImgSlot.appendChild(userImgBtn)
 
-    let userCity = document.createElement(`li`);
-    userCity.className = `list-group-item userCity`;
-    userCity.innerHTML = `<h5>City: ${userData.location.city}</h5>`;
+        let userCity = document.createElement(`li`);
+        userCity.className = `list-group-item userCity`;
+        userCity.innerHTML = `<h5>City: ${userData.location.city}</h5>`;
 
-    let userCountry = document.createElement(`li`);
-    userCountry.className = `list-group-item userCountry`;
-    userCountry.innerHTML = `<h5>Country: ${userData.location.country}</h5>`;
+        let userCountry = document.createElement(`li`);
+        userCountry.className = `list-group-item userCountry`;
+        userCountry.innerHTML = `<h5>Country: ${userData.location.country}</h5>`;
 
 
-    //groups ul individual data inside a div with a card class
+    //grouping up individual data inside a div with a card class
     user.appendChild(userName)
     user.appendChild(userImgSlot)
     user.appendChild(userCity)
     user.appendChild(userCountry)
     
-    //gets the user into a Bootstrap column for easier editing of style
+    //gets the card div into a Bootstrap column for easier editing of style
     mainSlot.appendChild(user)
-    //All columns of searched users get into a bootstarp ROW DIV 
+    //Placing the column inside a bootstarp ROW DIV in the HTML file with all the other user´s cards.
     searchedResult.appendChild(mainSlot)
 };
+
+function moreInfo(){
+
+    //obtaining and saving the data from the specificly selected user and sending it to the sesion storage.
+    //It uses the ID that was saved when the thumbnail is clicked as the index value where that user data is stored in the array.
+    sessionStorage.setItem("searchedUser",JSON.stringify(usersDatabase[userID]))
+    //opening the new tab with that user´s info
+    window.open("pages/moreInfo.html", "_blank");
+}
